a18767b3848f804f533ec44517034d0b21b53ed4
Brice Zele <bricezele1@gmail.com>
2024-07-22 18:45:49 +0200

    Animation on item changed

--- App.js
+++ App.js
@@ -1,29 +1,34 @@
 import React from "react";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from "./src/Home";
 import Splash from "./src/Splash";
 import Onboarding from "./components/Onboarding"
-import Setting from "./components/setting";
+import Setting from "./components/Setting";
 import Privacy from "./components/Privacy";
+import {initialWindowMetrics, SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
 
 
 const App = () => {
-  const Stack = createNativeStackNavigator();
+    const Stack = createNativeStackNavigator();
 
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Onboarding' screenOptions={{headerShown:false}}>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Splash" component={Splash} />
-        <Stack.Screen name="Onboarding" component={Onboarding} />
-        <Stack.Screen name="Setting" component={Setting} />
-        <Stack.Screen name="Privacy" component={Privacy} />
+    return (
+        <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+            <SafeAreaView style={{flex: 1}}>
+                <NavigationContainer>
+                    <Stack.Navigator initialRouteName='Onboarding' screenOptions={{headerShown: false}}>
+                        <Stack.Screen name="Home" component={Home}/>
+                        <Stack.Screen name="Splash" component={Splash}/>
+                        <Stack.Screen name="Onboarding" component={Onboarding}/>
+                        <Stack.Screen name="Setting" component={Setting}/>
+                        <Stack.Screen name="Privacy" component={Privacy}/>
 
 
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+                    </Stack.Navigator>
+                </NavigationContainer>
+            </SafeAreaView>
+        </SafeAreaProvider>
+    );
 };
 
 export default App;
---
+++ components/Setting.js
@@ -0,0 +1,184 @@
+import React, {useState} from 'react';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {AntDesign} from '@expo/vector-icons';
+import {useNavigation} from '@react-navigation/native';
+
+export default Setting = () => {
+    const navigation = useNavigation();
+    const [theme, setTheme] = useState('light'); // Initialize the theme to "light"
+
+    const handleImagePress = (imageNumber) => {
+        if (imageNumber == 2) {
+            setTheme('dark'); // Change the theme to "dark" when image 2 is clicked
+        } else {
+            setTheme('light'); // Reset the theme to "light" for other images
+        }
+    };
+
+    return (
+        <View style={[styles.container, theme == 'dark' ? styles.darkTheme : styles.lightTheme]}>
+            <TouchableOpacity onPress={() => navigation.goBack()} style={styles.button} activeOpacity={0.6}>
+                <AntDesign name="arrowleft" size={32} color="#FFB400"/>
+            </TouchableOpacity>
+            <View style={styles.childContainer}>
+                <Text style={styles.apparence}>Mode apparence</Text>
+                <Text style={styles.des}>Personnalisez votre theme pour une experience utilisateur</Text>
+                <Text style={styles.des}>unique</Text>
+                <View style={styles.imagesContainer}>
+                    <TouchableOpacity onPress={() => handleImagePress(1)}>
+                        <Image style={styles.image1} source={require('../assets/setting1.png')}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => handleImagePress(2)}>
+                        <Image style={styles.image} source={require('../assets/setting2.png')}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => handleImagePress(3)}>
+                        <Image style={styles.image} source={require('../assets/setting3.png')}/>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.langueContainer}>
+                    <Text style={[styles.text, styles.langue]}>Langue</Text>
+                    <TouchableOpacity onPress={() => handleImagePress(1)}>
+                        <Text style={styles.langueSelect}>Francais</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={[styles.langueContainer, {marginTop: 10}]}>
+                    <Text style={styles.notif}>Notification</Text>
+                    <TouchableOpacity onPress={() => handleImagePress(1)}>
+                        <Text style={styles.notifselect}>Activer</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity onPress={() => navigation.navigate('Privacy')} style={styles.buttonA}>
+                        <Text style={styles.buttonText}>Appliquer</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'white',
+        flex: 1,
+    },
+    text: {
+        fontFamily: 'Montserrat',
+    },
+    lightTheme: {
+        backgroundColor: 'white',
+    },
+    darkTheme: {
+        backgroundColor: 'black',
+    },
+    apparence: {
+        fontWeight: '500',
+        fontSize: 24,
+    },
+    imagesContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '100%',
+    },
+    image: {
+        width: 360 / 3 - 20, // 20 is the spacing between images
+        height: 300,
+        resizeMode: 'contain',
+        marginRight: 20,
+        shadowColor: '#000',
+        shadowOffset: {width: 0, height: 2},
+        shadowOpacity: 0.5,
+        shadowRadius: 4,
+
+    },
+    image1: {
+        width: 353 / 3 - 20, // 20 is the spacing between images
+        height: 290,
+        resizeMode: 'contain',
+        marginRight: 20,
+        shadowColor: '#000',
+        shadowOffset: {width: 0, height: 2},
+        shadowOpacity: 0.5,
+        shadowRadius: 4,
+    },
+    des: {
+        fontSize: 12,
+        width: '100%',
+        flexWrap: 'wrap',
+        color: '#62656b',
+    },
+    langueContainer:
+        {
+            flexDirection: 'row',
+            width: '100%',
+            justifyContent: "space-between"
+        },
+    langue:
+        {
+            fontWeight: '400',
+            fontSize: 16,
+            marginRight: 165,
+            fontFamily: 'Montsera',
+        },
+    langueSelect:
+        {
+            fontWeight: '500',
+            fontSize: 16,
+            color: '#FFB400',
+
+        },
+    notifContainer:
+        {
+            flexDirection: 'row',
+            marginTop: 20,
+        },
+    notif:
+        {
+            fontWeight: '400',
+            fontSize: 16,
+            fontFamily: 'Montsera',
+            marginRight: 158,
+        },
+    notifselect:
+        {
+            fontFamily: 'Montsera',
+            fontWeight: '500',
+            fontSize: 16,
+            color: '#FFB400',
+
+        },
+    buttonContainer:
+        {
+            marginTop: 30,
+            marginVertical: -80,
+        },
+    buttonA: {
+        backgroundColor: '#FFB400',
+        padding: 15,
+        paddingHorizontal: 70,
+        borderRadius: 5,
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    },
+    buttonText: {
+        fontSize: 16,
+        color: '#fff',
+        fontWeight: '500'
+    },
+    button: {
+        padding: 10,
+        borderRadius: 5,
+        backgroundColor: '#fff',
+
+        // shadowColor: '#000',
+        // shadowOffset: { width: 0, height: 2 },
+        // shadowOpacity: 0.5,
+        // shadowRadius: 4,
+    }, childContainer: {
+        flex: 1,
+        padding: 20
+    }
+
+
+});
